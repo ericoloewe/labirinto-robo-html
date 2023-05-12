f496c19ed2a03c376a1a2d6787a8817b20e81fbe
Erico de Souza Loewe <ericoloewe_dbc@getnet.com.br>
2023-05-12 14:45:01 -0300

    nextjs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
+const isExport = process.argv.some(a => a.includes('export'));
+const isDev = process.argv.some(a => a.includes('dev'));
+const basePath = isDev ? '' : '/labirinto-robo-html';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: basePath,
+  assetPrefix: `${basePath}/`,
+  env: {
+    BASE_PATH: basePath
+  },
   webpack: (config, { dev }) => {
     config.module.rules.push({
       test: /\.txt$/,
